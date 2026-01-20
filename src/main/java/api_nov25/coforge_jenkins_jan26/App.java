df41372a99af3c998c6380e44b6d710de0e11817
priyanka <priyankarr140@gmail.com>
2026-01-20 14:34:21 +0530

    webhook

---
+++ src/main/java/api_nov25/coforge_jenkins_jan26/App.java
@@ -0,0 +1,10 @@
+package api_nov25.coforge_jenkins_jan26;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/api_nov25/coforge_jenkins_jan26/LoginTest.java
@@ -0,0 +1,10 @@
+package api_nov25.coforge_jenkins_jan26;
+import org.testng.annotations.*;
+public class LoginTest {
+
+	@Test
+	void login()
+	{
+		System.out.println("LOGIN****");
+	}
+}
